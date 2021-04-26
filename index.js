bb23096c2815dbcc733148e83633347fd1664cdf
Kristian Pedersen <kristian.pedersen@tietoevry.com>
2021-04-26 09:27:27 +0200

    .

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const nirKodegruppe = 15;
 const socioKodegruppe = 25023;
 const lopenummer = 51;
 
-JSON.parse(fs.readFileSync("./nirkoder2021.json", "utf-8")).Result
-	.filter(nir => nir.Kodeverk === nirKodegruppe)
-	.forEach((nirkode, i) => {
-		const { Navn, Kode, Beskrivelse } = nirkode;
-		const CSVLinje = `${socioKodegruppe},${lopenummer + i},0,"2021 - ${Navn}",0,0,3,0,"${Kode}","${Beskrivelse}",0,0,0,0,0`;
-		console.log(CSVLinje);
-	})
+const result = JSON.parse(fs.readFileSync("./nirkoder2021.json", "utf-8")).Result
+const filtered = result.filter(nir => nir.Kodeverk === nirKodegruppe)
+
+filtered.forEach((nirkode, i) => {
+	const { Navn, Kode, Beskrivelse } = nirkode;
+	const CSVLinje = `${socioKodegruppe},${lopenummer + i},0,"2021 - ${Navn}",0,0,3,0,"${Kode}","${Beskrivelse}",0,0,0,0,0`;
+	console.log(CSVLinje);
+});
